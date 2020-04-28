7e93fd921fdd80d40eecde60fae7d6ea9b3c055d
Your Name <you@example.com>
2020-04-28 13:07:42 +0800

    initial commit

---
+++ src/components/TaskTable.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id = "task-table">
+		<table>
+			<thead>
+				<tr>
+					<td>Task name</td>
+					<td>Date Due</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>sample task 1</td>
+					<td>sample date 1</td>
+				</tr>		
+				<tr>
+					<td>sample task 2</td>
+					<td>sample date 2</td>
+				</tr>
+				<tr>
+					<td>sample task 3</td>
+					<td>sample date 3</td>
+				</tr>
+			</tbody>
+		</table>
+</template>
+
+<script>
+	export default {
+		name: 'task-table',
+	}
+</script>
+
+<style scoped>
+	
+</style>
